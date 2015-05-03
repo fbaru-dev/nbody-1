5925cc13f2d6a2a79d11539bf0785f9f49896e1c
jodavies <joshdavies@hotmail.co.uk>
2015-05-03 19:42:47 +0100

    Added manually vectorized ComputeAccel routine.

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # nbody
 n-body solver, with the aim of writing a fast MPI implementation.
 
-For n-body, this is a little non-trivial since each timestep the position of every body is needed, so it is something of a communication-heavy problem. Machine available for testing has only a Gigabit Ethernet interconnect, so efficient communication will be essential.
+For the n-body problem this is a little non-trivial since each timestep the position of every body is needed, so it is something of a communication-heavy problem. Cluster available for testing has only a Gigabit Ethernet interconnect, so efficient communication will be essential.
 
 
 
 ### Serial Implementation
 Particle position, velocity, acceleration and mass are each stored in separate arrays rather than a somewhat neater struct approach, to make it easier for the auto-vectorizer and to use manual vector intrinsics.
 
-
+#### Manual Vectorization
+Compute accelerations for 4 particles at a time in inner loop of `ComputeAccel`, routine called `ComputeAccelAVX`. This is just over twice as fast as the autovectorized version.
--- src/nbody.c
+++ src/nbody.c
@@ -8,8 +8,8 @@
 
 // Algorithm parameters
 #define G 0.1
-#define NPARTICLES 500
-#define NTIMESTEPS 50000
+#define NBODIES 1000
+#define NTIMESTEPS 5000
 #define STEPSIZE 0.0001
 #define FORCELIMIT 0.0001
 
@@ -35,6 +35,12 @@ void TimeStep(
 	const real_t * restrict mass);
 
 void ComputeAccel(
+	const real_t * restrict rx,
+	const real_t * restrict ry,
+	real_t * restrict ax,
+	real_t * restrict ay,
+	const real_t * restrict mass);
+void ComputeAccelAVX(
 	const real_t * restrict rx,
 	const real_t * restrict ry,
 	real_t * restrict ax,
@@ -85,13 +91,13 @@ int main(void)
 	real_t * ax;
 	real_t * ay;
 	real_t * mass;
-	rx = _mm_malloc(NPARTICLES * sizeof *rx,32);
-	ry = _mm_malloc(NPARTICLES * sizeof *ry,32);
-	vx = _mm_malloc(NPARTICLES * sizeof *vx,32);
-	vy = _mm_malloc(NPARTICLES * sizeof *vy,32);
-	ax = _mm_malloc(NPARTICLES * sizeof *ax,32);
-	ay = _mm_malloc(NPARTICLES * sizeof *ay,32);
-	mass = _mm_malloc(NPARTICLES * sizeof *mass,32);
+	rx = _mm_malloc(NBODIES * sizeof *rx,32);
+	ry = _mm_malloc(NBODIES * sizeof *ry,32);
+	vx = _mm_malloc(NBODIES * sizeof *vx,32);
+	vy = _mm_malloc(NBODIES * sizeof *vy,32);
+	ax = _mm_malloc(NBODIES * sizeof *ax,32);
+	ay = _mm_malloc(NBODIES * sizeof *ay,32);
+	mass = _mm_malloc(NBODIES * sizeof *mass,32);
 
 	SetInitialConditions(rx,ry, vx,vy, ax,ay, mass);
 
@@ -105,10 +111,10 @@ int main(void)
 	fprintf(plotfile, "set xrange [-100:100]\n");
 	fprintf(plotfile, "set yrange [-100:100]\n");
 	fprintf(plotfile, "plot \\\n");
-	for (int i = 0; i < NPARTICLES-1; i++) {
+	for (int i = 0; i < NBODIES-1; i++) {
 		fprintf(plotfile, "\"pos.dat\" using %d:%d with linespoints,\\\n", 2*i+1, 2*i+2);
 	}
-	fprintf(plotfile, "\"pos.dat\" using %d:%d with linespoints\n", 2*(NPARTICLES-1)+1, 2*(NPARTICLES-1)+2);
+	fprintf(plotfile, "\"pos.dat\" using %d:%d with linespoints\n", 2*(NBODIES-1)+1, 2*(NBODIES-1)+2);
 #endif
 
 
@@ -118,7 +124,7 @@ int main(void)
 		TimeStep(datfile, n, rx,ry, vx,vy, ax,ay, mass);
 	}
 	timeElapsed = GetWallTime() - timeElapsed;
-	printf("MegaUpdates/second: %lf. Error: %le\n", NTIMESTEPS*NPARTICLES/timeElapsed/1000000.0, ErrorCheck(rx));
+	printf("MegaUpdates/second: %lf. Error: %le\n", NTIMESTEPS*NBODIES/timeElapsed/1000000.0, ErrorCheck(rx));
 
 
 
@@ -147,7 +153,7 @@ void TimeStep(
 	real_t * restrict ay,
 	const real_t * restrict mass)
 {
-	ComputeAccel(rx,ry, ax,ay, mass);
+	ComputeAccelAVX(rx,ry, ax,ay, mass);
 	UpdatePositions(rx,ry, vx,vy, ax,ay);
 #ifdef PRINTPOS
 	if (timeStep % 1000 == 0) {
@@ -166,8 +172,8 @@ void ComputeAccel(
 {
 	double distx, disty, sqrtRecipDist;
 
-	for (int i = 0; i < NPARTICLES; i++) {
-		for (int j = i+1; j < NPARTICLES; j++) {
+	for (int i = 0; i < NBODIES; i++) {
+		for (int j = i+1; j < NBODIES; j++) {
 			distx = rx[i] - rx[j];
 			disty = ry[i] - ry[j];
 			sqrtRecipDist = 1.0/sqrt(distx*distx+disty*disty);
@@ -176,12 +182,113 @@ void ComputeAccel(
 			ax[j] -= (mass[i] * distx * sqrtRecipDist*sqrtRecipDist*sqrtRecipDist);
 			ay[j] -= (mass[i] * disty * sqrtRecipDist*sqrtRecipDist*sqrtRecipDist);
 
-			// This version with a force-limiting term stops nearby particles experiencing arbitrarily high
+			// This version with a force-limiting term stops nearby bodies experiencing arbitrarily high
 			// forces. Important for numerical stability, but not for performance testing.
 //			ax[i] += (mass[j] * distx * pow(sqrtRecipDist*sqrtRecipDist+FORCELIMIT,3.0/2.0));
 //			ay[i] += (mass[j] * disty * pow(sqrtRecipDist*sqrtRecipDist+FORCELIMIT,3.0/2.0));
 //			ax[j] -= (mass[i] * distx * pow(sqrtRecipDist*sqrtRecipDist+FORCELIMIT,3.0/2.0));
 //			ay[j] -= (mass[i] * disty * pow(sqrtRecipDist*sqrtRecipDist+FORCELIMIT,3.0/2.0));
+		}
+	}
+
+}
+
+
+void ComputeAccelAVX(
+	const real_t * restrict rx,
+	const real_t * restrict ry,
+	real_t * restrict ax,
+	real_t * restrict ay,
+	const real_t * restrict mass)
+{
+	double distx, disty, sqrtRecipDist;
+
+	// limit of vectorized loop is the multiple of for including or below NBODIES
+	// May have to "clean up" a few bodies at the end
+	const int jVecMax = 4*(NBODIES/4);
+
+	for (int i = 0; i < NBODIES; i++) {
+
+		// Vectorized j loop starts at multiple of 4 >= i+1
+		const int jVecMin = (4*((i)/4)+4) > NBODIES ? NBODIES : (4*((i)/4)+4);
+
+		// first initial non-vectorized part
+		for (int j = i+1; j < jVecMin; j++) {
+			distx = rx[i] - rx[j];
+			disty = ry[i] - ry[j];
+			sqrtRecipDist = 1.0/sqrt(distx*distx+disty*disty);
+			ax[i] += (mass[j] * distx * sqrtRecipDist*sqrtRecipDist*sqrtRecipDist);
+			ay[i] += (mass[j] * disty * sqrtRecipDist*sqrtRecipDist*sqrtRecipDist);
+			ax[j] -= (mass[i] * distx * sqrtRecipDist*sqrtRecipDist*sqrtRecipDist);
+			ay[j] -= (mass[i] * disty * sqrtRecipDist*sqrtRecipDist*sqrtRecipDist);
+		}
+
+		// if we have already finished, break out of the loop early
+		if (jVecMax < jVecMin) break;
+
+
+		// main vectorized part
+		__m256d rxiVec = _mm256_set1_pd(rx[i]);
+		__m256d ryiVec = _mm256_set1_pd(ry[i]);
+		__m256d massiVec = _mm256_set1_pd(mass[i]);
+		__m256d axiUpdVec = _mm256_set1_pd(0.0);
+		__m256d ayiUpdVec = _mm256_set1_pd(0.0);
+
+		for (int j = jVecMin; j < jVecMax; j+=4) {
+			__m256d rxjVec = _mm256_load_pd(&rx[j]);
+			__m256d ryjVec = _mm256_load_pd(&ry[j]);
+			__m256d axjVec = _mm256_load_pd(&ax[j]);
+			__m256d ayjVec = _mm256_load_pd(&ay[j]);
+			__m256d massjVec = _mm256_load_pd(&mass[j]);
+
+			__m256d distxVec = _mm256_sub_pd(rxiVec, rxjVec);
+			__m256d distyVec = _mm256_sub_pd(ryiVec, ryjVec);
+
+			__m256d sqrtRecipDistVec = _mm256_div_pd(_mm256_set1_pd(1.0),
+			                                         _mm256_sqrt_pd(_mm256_add_pd(_mm256_mul_pd(distxVec,distxVec),
+			                                                                      _mm256_mul_pd(distyVec,distyVec))));
+
+			axiUpdVec = _mm256_add_pd(axiUpdVec,_mm256_mul_pd(massjVec,
+			                                                  _mm256_mul_pd(distxVec,
+			                                                                _mm256_mul_pd(sqrtRecipDistVec,
+			                                                                              _mm256_mul_pd(sqrtRecipDistVec,
+			                                                                                            sqrtRecipDistVec)))));
+			ayiUpdVec = _mm256_add_pd(ayiUpdVec,_mm256_mul_pd(massjVec,
+			                                                  _mm256_mul_pd(distyVec,
+			                                                                _mm256_mul_pd(sqrtRecipDistVec,
+			                                                                              _mm256_mul_pd(sqrtRecipDistVec,
+			                                                                                            sqrtRecipDistVec)))));
+			axjVec = _mm256_sub_pd(axjVec,_mm256_mul_pd(massiVec,
+			                                                  _mm256_mul_pd(distxVec,
+			                                                                _mm256_mul_pd(sqrtRecipDistVec,
+			                                                                              _mm256_mul_pd(sqrtRecipDistVec,
+			                                                                                            sqrtRecipDistVec)))));
+			ayjVec = _mm256_sub_pd(ayjVec,_mm256_mul_pd(massiVec,
+			                                                  _mm256_mul_pd(distyVec,
+			                                                                _mm256_mul_pd(sqrtRecipDistVec,
+			                                                                              _mm256_mul_pd(sqrtRecipDistVec,
+			                                                                                            sqrtRecipDistVec)))));
+
+			_mm256_store_pd(&ax[j],axjVec);
+			_mm256_store_pd(&ay[j],ayjVec);
+		}
+
+		// Now need to sum elements of axiUpdVec,ayiUpdVec and add to ax[i],ay[i]
+		axiUpdVec = _mm256_hadd_pd(axiUpdVec,axiUpdVec);
+		ayiUpdVec = _mm256_hadd_pd(ayiUpdVec,ayiUpdVec);
+		ax[i] += ((double*)&axiUpdVec)[0] + ((double*)&axiUpdVec)[2];
+		ay[i] += ((double*)&ayiUpdVec)[0] + ((double*)&ayiUpdVec)[2];
+
+
+		// final non-vectorized part, iff we didn't already run up to a jVecMin which is larger than jVecMax
+		for (int j = jVecMax; j < NBODIES; j++) {
+			distx = rx[i] - rx[j];
+			disty = ry[i] - ry[j];
+			sqrtRecipDist = 1.0/sqrt(distx*distx+disty*disty);
+			ax[i] += (mass[j] * distx * sqrtRecipDist*sqrtRecipDist*sqrtRecipDist);
+			ay[i] += (mass[j] * disty * sqrtRecipDist*sqrtRecipDist*sqrtRecipDist);
+			ax[j] -= (mass[i] * distx * sqrtRecipDist*sqrtRecipDist*sqrtRecipDist);
+			ay[j] -= (mass[i] * disty * sqrtRecipDist*sqrtRecipDist*sqrtRecipDist);
 		}
 	}
 
@@ -195,7 +302,7 @@ void UpdatePositions(
 	real_t * restrict ax,
 	real_t * restrict ay)
 {
-	for (int i = 0; i < NPARTICLES; i++) {
+	for (int i = 0; i < NBODIES; i++) {
 			//new force values in .ax, .ay
 			//update pos and vel
 			vx[i] += (-G)*STEPSIZE * ax[i];
@@ -220,7 +327,7 @@ void SetInitialConditions(
 	real_t * restrict mass)
 {
 	// Set random initial conditions.
-	for (int i = 0; i < NPARTICLES; i++) {
+	for (int i = 0; i < NBODIES; i++) {
 		rx[i] = ((double)rand()*(double)100/(double)RAND_MAX)*pow(-1,rand()%2);
 		ry[i] = ((double)rand()*(double)100/(double)RAND_MAX)*pow(-1,rand()%2);
 		vx[i] = (rand()%3)*pow(-1,rand()%2);
@@ -235,7 +342,7 @@ double ErrorCheck(const real_t * restrict rx)
 {
 	// Compute sum of x coordinates. Can use to check consistency between versions.
 	double sumx = 0;
-	for (int i = 0; i < NPARTICLES; i++) {
+	for (int i = 0; i < NBODIES; i++) {
 		sumx += rx[i];
 	}
 	return sumx;
@@ -244,10 +351,10 @@ double ErrorCheck(const real_t * restrict rx)
 
 void PrintPositions(FILE * file, const real_t * restrict rx, const real_t * restrict ry)
 {
-	for(int i = 0; i < NPARTICLES-1; i++) {
+	for(int i = 0; i < NBODIES-1; i++) {
 		fprintf(file, "%le %le ", rx[i],ry[i]);
 	}
-	fprintf(file, "%le %le\n",rx[NPARTICLES-1],ry[NPARTICLES-1]);
+	fprintf(file, "%le %le\n",rx[NBODIES-1],ry[NBODIES-1]);
 }
 
 
